cfd01b2b948959e360b74f0325bee504c11a3408
Zachary Krantz <122484628+ZachK03@users.noreply.github.com>
2023-04-22 02:08:21 -0400

    Add files via upload

--- src/module-info.java
+++ src/module-info.java
@@ -3,7 +3,10 @@ module TextAnalyzerWithGUI {
 	requires javafx.graphics;
 	requires javafx.fxml;
 	requires javafx.base;
-	
+	requires org.jsoup;
+	requires org.junit.jupiter.api;
+	requires junit;
+	requires org.junit.platform.suite.api;
 	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
